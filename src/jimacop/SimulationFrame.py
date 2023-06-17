66623325193913d60148fd2411ad495ebe22c803
kudoryutaro <ryutaro.kudo.p2@dc.tohoku.ac.jp>
2023-06-17 14:19:19 +0900

    added SimulationFrame class and SimulationFrames class

---
+++ src/jimacop/SimulationFrame.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+
+class SimulationFrame(
+
+):
+    """シミュレーションしたデータを読み込み、書き込み、分析するためのクラス
+    一つのフレームを扱う
+
+    Attributes
+    ----------
+    atoms : pd.DataFrame
+        原子のtype, 座標, 速度, 加速度, 力, type, 電荷などを含むpandasのDataFrame
+        原子のタイプは1-indexed
+    cell : np.array
+        cellの大きさが入ったarray, shape:[3]
+        cell[0]:x方向, cell[1]:y方向, cell[2]:z方向
+    atom_symbol_to_type : dict[str, int]
+        原子のシンボルをkey, 原子のtypeをvalueとするdict
+    atom_type_to_symbol : dict[int, str]
+        原子のtypeをkey, 原子のシンボルをvalueとするdict
+    atom_type_to_mass : dict[int, float]
+        原子のtypeをkey, 原子の質量(g/mol)をvalueとするdict
+    step_num: int
+        MDした時のこのSimulationFrameが持つステップ数
+    potential_energy: int
+        このフレームが持つポテンシャルエネルギー, 単位はeV
+    """
+    atoms: pd.DataFrame
+    cell: np.array # shape:[3]
+    atom_symbol_to_type: dict[str, int]
+    atom_type_to_symbol : dict[int, str]
+    atom_type_to_mass : dict[int, float]
+    step_num: int
+
+    def __init__(self):
+        pass
+
