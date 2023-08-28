c7b428b9a05ddb43a77c5d6d40a0407848b562cd
kudoryutaro <ryutaro.kudo.p2@dc.tohoku.ac.jp>
2023-08-28 14:20:15 +0900

    added sf.allegro(), sfs.allegro(), sfs.concat_force_and_pred_force(), sfs.concat_pot_and_pred_pot()

--- src/limda/SimulationFrame.py
+++ src/limda/SimulationFrame.py
@@ -43,6 +43,8 @@ class SimulationFrame(
     atom_type_to_symbol : dict[int, str]
     atom_type_to_mass : dict[int, float]
     step_num: int
+    potential_energy: float
+    pred_potential_energy: float
 #--------------------------------------
     def __init__(self, para: str = ""):
         self.atoms = None
@@ -52,6 +54,7 @@ class SimulationFrame(
         self.atom_type_to_mass = None
         self.step_num = None
         self.potential_energy = None
+        self.pred_potential_energy = None
         if para:
             self.import_para_from_str(para)
 #-------------------------------------
--- src/limda/SimulationFrames.py
+++ src/limda/SimulationFrames.py
@@ -4,6 +4,8 @@ import pathlib
 import random
 import os
 from tqdm import tqdm, trange
+from typing import Union
+import torch
 from .import_frames import ImportFrames
 from .export_frames import ExportFrames
 from .SimulationFrame import SimulationFrame
@@ -147,3 +149,109 @@ class SimulationFrames(
             sfs_list.append(remain_sfs)
         return sfs_list
 
+#---------------------------------------------------------------------------------------------------------------
+    def allegro(self,
+                cut_off: float,
+                device: Union[str, torch.DeviceObjType],
+                allegro_model: torch.jit._script.RecursiveScriptModule,
+                ) -> None:
+        """sfsのもつすべてのSimulationFrameに対して、以下を行う
+        Allegroを使って、sfに入っている原子の座標に対して推論を行い、
+        ポテンシャルエネルギーと原子に働く力を計算する
+        Allegroによって予測されたポテンシャルエネルギーはsf.pred_potential_energyに入る
+        Allegroによって予測されたそれぞれの原子にかかる力はsf.atoms.loc[:, ["pred_fx", "pred_fy", "pred_fz"]]に入る
+        Parameters
+        ----------
+            cut_off: float
+                原子の相互作用のカットオフ半径
+            device: Union[str, torch.DeviceObjType]
+                どのデバイスでAllegroの計算を行うか, "cpu" or "cuda"
+            allegro_model: torch.jit._script.RecursiveScriptModule
+                frozenされたAllegroを読み込んだモデル
+                pathではないことに注意
+        """
+        for frame_idx in range(len(self.sf)):
+            self.sf[frame_idx].allegro(cut_off=cut_off, device=device, allegro_model=allegro_model)
+
+    
+    def concat_force_and_pred_force(self, 
+                                    reduce_direction: bool = False,
+                                    ) -> pd.DataFrame:
+        """それぞれのSimulationFrameが持つ力とAllegroによって予測された力という一つのDataFrameを作る
+        Parameters
+        ----------
+            reduce_direction: bool = False
+                x, y, z方向をまとめるかどうか
+                reduce_direction == Trueのときは、返り値のDataFrameのcolumnは["f", "pred_f"]
+
+                reduce_direction == Falseのときは、返り値のDataFrameのcolumnは
+                ["fx", "fy", "fz", "pred_fx", "pred_fy", "pred_fz"]
+        Returns
+        -------
+            force_and_pred_force_reduced または force_and_pred_forceが返る
+            force_and_pred_force_reduced : pd.DataFrame
+                columnは["f", "pred_f"]、行は同じ原子の同じ方向に働く力に対応する
+            force_and_pred_force : pd.DataFrame
+                columnは["fx", "fy", "fz", "pred_fx", "pred_fy", "pred_fz"]、行は同じ原子の同じ方向に働く力に対応する
+
+        Usage
+        -----
+            横軸に第一原理計算で計算された力, 縦軸にAllegroで予測された力というプロットと作りたいときに用いる
+            ```python
+            sfs = SimulationFrames("C H O N Si")
+            sfs.import_allegro_frames("/path/to/allegro_frame.pickle")
+            device = torch.device("cuda")
+            model = torch.jit.load("/path/to/allegro_frozen_1000.pth")
+            model.to(device)
+            sfs.allegro(cut_off=3.4, allegro_model=model, device=device)
+            force_and_pred_force = sfs.concat_force_and_pred_force(reduce_direction=True)
+            plt.scatter(force_and_pred_force.loc[:, "f"], force_and_pred_force.loc[:, "pred_f"], alpha=0.005)
+            plt.show()
+            ```
+        """
+        force_and_pred_force_list = []
+        for frame_idx in range(len(self.sf)):
+            force_and_pred_force_list.append(
+                self.sf[frame_idx].atoms.loc[:,
+                    ["fx", "fy", "fz", "pred_fx", "pred_fy", "pred_fz"]
+                ]
+            )
+        force_and_pred_force = pd.concat(force_and_pred_force_list)
+
+        if reduce_direction:
+            force_and_pred_force_reduced = pd.DataFrame(
+                np.concatenate(
+                    [force_and_pred_force.loc[:, ["fx", "pred_fx"]].values,
+                    force_and_pred_force.loc[:, ["fy", "pred_fy"]].values,
+                    force_and_pred_force.loc[:, ["fz", "pred_fz"]].values,],
+                    axis=0),
+                columns=["f", "pred_f"]
+            )
+            return force_and_pred_force_reduced
+        else:
+            return force_and_pred_force
+    
+    def concat_pot_and_pred_pot(self) -> pd.DataFrame:
+        """それぞれのSimulationFrameが持つポテンシャルエネルギーと
+        Allegroによって予測されたポテンシャルエネルギーという一つのDataFrameを作る
+        Returns
+        -------
+            pot_and_pred_pot : pd.DataFrame
+                columnは["potential_energy", "pred_potential_energy"]、行は同じフレームに対応する
+        Usage
+        -----
+            横軸に第一原理計算で計算されたポテンシャルエネルギー, 
+            縦軸にAllegroで予測されたポテンシャルエネルギーというプロットと作りたいときに用いる
+        """
+        potential_energy_list = []
+        pred_potential_energy_list = []
+        for frame_idx in range(len(self.sf)):
+            potential_energy_list.append(self.sf[frame_idx].potential_energy)
+            pred_potential_energy_list.append(self.sf[frame_idx].pred_potential_energy)
+
+        pot_and_pred_pot = pd.DataFrame({
+                "potential_energy":potential_energy_list,
+                "pred_potential_energy":pred_potential_energy_list
+            })
+
+        return pot_and_pred_pot
