1c1feff7fb3592fdf9946f668a5218d683da7e58
knak-9 <kai.nakajima.q2@dc.tohoku.ac.jp>
2023-10-27 20:40:59 +0900

    neighbor周りをすっきりさせた

--- src/limda/analize_frame.py
+++ src/limda/analize_frame.py
@@ -2,13 +2,183 @@ from .neighbor import cy_get_neighbor_list
 from .neighbor import cy_count_molecules
 from .neighbor import cy_count_bonds
 from .neighbor import cy_count_coord_numbers
+from .neighbor import get_neighbor_list_using_cython
+from .neighbor import get_edge_idx_using_cython
+from .neighbor import count_molecules_using_cython
+from .neighbor import count_bonds_using_cython
+from .neighbor import count_coord_numbers_using_cython
 
 class AnalizeFrame(
 
 ):
     def __init__(self):
         pass
+#---------------------------------------------------------------------------------------------------------    
+    def set_neighbor_info(self,
+        mode : str = "",
+        cut_off : float = None,
+        bond_length : list[list[float]] = []
+        )->[list[list[float]], float]:
+        assert mode == 'B' or mode == 'C', "Please configure mode"
+        atom_type_num = len(self.atom_symbol_to_type)
+        if mode == 'B':
+            if not bond_length:
+                if "bond_length" in self.limda_default:
+                    bond_length = self.limda_default["bond_length"]
+            assert len(bond_length) == atom_type_num, "Incorrect format of bond length"
+            for bond_list in bond_length:
+                assert len(bond_list) == atom_type_num, "Incorrect format of bond length"
+        elif mode == 'C':
+            if cut_off is None:
+                if "cut_off" in self.limda_default:
+                    cut_off = self.limda_default["cut_off"]
+            bond_length = [[cut_off for _ in range(atom_type_num)] for __ in range(atom_type_num)]
 
+        mesh_length = max(list(map(lambda x: max(x), bond_length))) + 0.01 # cut_off(bond_length) + margin
+        if mesh_length*3 > min(self.cell):
+            mesh_length = min(self.cell)/3
+
+        return bond_length, mesh_length
+#---------------------------------------------------------------------------------------------------------
+    def Get_neighbor_list(
+        self,
+        mode : str = "",
+        cut_off = None,
+        bond_length : list[list[float]] = []
+        )->list[list[int]]:
+        """
+        """
+        bond_length, mesh_length = self.set_neighbor_info(mode, cut_off, bond_length)
+        neighbor_list = get_neighbor_list_using_cython(atoms_type = self.atoms["type"],
+                                                       atoms_pos = [self.atoms["x"], self.atoms["y"], self.atoms["z"]],
+                                                       mesh_length = mesh_length,
+                                                       atom_num = len(self),
+                                                       bond_length = bond_length,
+                                                       cell = self.cell)
+        return neighbor_list
+#---------------------------------------------------------------------------------------------------------    
+    def Get_edge_idx(
+        self,
+        mode : str = "C",
+        cut_off = None,
+        bond_length : list[list[float]] = []
+        ):
+        bond_length, mesh_length = self.set_neighbor_info(mode, cut_off, bond_length)
+        edge_idx = get_edge_idx_using_cython(atoms_type = self.atoms["type"],
+                                                       atoms_pos = [self.atoms["x"], self.atoms["y"], self.atoms["z"]],
+                                                       mesh_length = mesh_length,
+                                                       atom_num = len(self),
+                                                       bond_length = bond_length,
+                                                       cell = self.cell)
+        return edge_idx
+#---------------------------------------------------------------------------------------------------------
+    def Count_molecules(
+        self,
+        mode : str = "",
+        cut_off = None,
+        bond_length : list[list[float]] = []
+        ):
+        bond_length, mesh_length = self.set_neighbor_info(mode, cut_off, bond_length)
+        molecules_list = count_molecules_using_cython(atoms_type = self.atoms["type"],
+                                                       atoms_pos = [self.atoms["x"], self.atoms["y"], self.atoms["z"]],
+                                                       mesh_length = mesh_length,
+                                                       atom_num = len(self),
+                                                       bond_length = bond_length,
+                                                       cell = self.cell,
+                                                       typ_len = len(self.atom_symbol_to_type))
+        molecules_dict :dict[str, int] = {}
+        for molecule in molecules_list:
+            mol_str = ""
+            for i, atom_number in enumerate(molecule):
+                if not atom_number:
+                    continue
+                if mol_str:
+                    mol_str = f"{mol_str} "
+                mol_str = f"{mol_str}{self.atom_type_to_symbol[i+1]}{atom_number}"
+            if mol_str not in molecules_dict.keys():
+                molecules_dict[mol_str] = 1
+            else:
+                molecules_dict[mol_str] += 1
+        return molecules_dict
+#---------------------------------------------------------------------------------------------------------
+    def Count_bonds(
+        self,
+        mode : str = "",
+        cut_off = None,
+        bond_length : list[list[float]] = []
+        ):
+        bond_length, mesh_length = self.set_neighbor_info(mode, cut_off, bond_length)
+        bonds_list = count_bonds_using_cython(atoms_type = self.atoms["type"],
+                                                       atoms_pos = [self.atoms["x"], self.atoms["y"], self.atoms["z"]],
+                                                       mesh_length = mesh_length,
+                                                       atom_num = len(self),
+                                                       bond_length = bond_length,
+                                                       cell = self.cell,
+                                                       typ_len = len(self.atom_symbol_to_type))
+        bonds_dict: dict[str,int] = {}
+        for typ_i_idx in range(len(self.atom_symbol_to_type)):
+            for typ_j_idx in range(typ_i_idx, len(self.atom_symbol_to_type)):
+                bond_str = f"{self.atom_type_to_symbol[typ_i_idx + 1]}-{self.atom_type_to_symbol[typ_j_idx + 1]}"
+                bonds_dict[bond_str] = bonds_list[typ_i_idx][typ_j_idx]    
+        return bonds_dict
+#---------------------------------------------------------------------------------------------------------   
+    def Count_coord_numbers(
+        self,
+        mode : str = "",
+        cut_off = None,
+        bond_length : list[list[float]] = []
+        ):
+        bond_length, mesh_length = self.set_neighbor_info(mode, cut_off, bond_length)
+        coord_numbers_list = count_coord_numbers_using_cython(atoms_type = self.atoms["type"],
+                                                       atoms_pos = [self.atoms["x"], self.atoms["y"], self.atoms["z"]],
+                                                       mesh_length = mesh_length,
+                                                       atom_num = len(self),
+                                                       bond_length = bond_length,
+                                                       cell = self.cell,
+                                                       typ_len = len(self.atom_symbol_to_type))
+        coord_numbers_dict: list[dict[int,int]] = [{} for _ in range(len(self.atom_symbol_to_type))]
+        for typ_idx in range(len(self.atom_symbol_to_type)):
+            for coord_number in coord_numbers_list[typ_idx]:
+                if coord_number not in coord_numbers_dict[typ_idx].keys():
+                    coord_numbers_dict[typ_idx][coord_number] = 1
+                else:
+                    coord_numbers_dict[typ_idx][coord_number] += 1
+            coord_numbers_dict[typ_idx] = dict(sorted(coord_numbers_dict[typ_idx].items()))
+        return coord_numbers_dict   
+#---------------------------------------------------------------------------------------------------------
+    def get_neighbor_list_test(self, bond_length: list[list[float]])->list[list[int]]:
+        """ neighbor_listを作成します。
+            pythonでO(N^2)のため、get_neighbor_list()のtest用です。
+        Parameters
+        -----------
+            bond_length: list[list[float]]
+        """
+        neighbor_list_test = [[] for _ in range(len(self))]
+        x = self.atoms['x'].values
+        y = self.atoms['y'].values
+        z = self.atoms['z'].values
+        atom_types = self.atoms['type'].to_list()
+        for i  in range(self.get_total_atoms()):
+            for j in range(i+1, self.get_total_atoms()):
+                dx:list[float] = [None,None,None]
+                dx[0] = x[j] - x[i]
+                dx[1] = y[j] - y[i]
+                dx[2] = z[j] - z[i]
+                for ax in range(3):
+                    if dx[ax] < -self.cell[ax]/2:
+                        dx[ax] += self.cell[ax]
+                    elif self.cell[ax]/2 < dx[ax]:
+                        dx[ax] -= self.cell[ax]
+                if dx[0]*dx[0] + dx[1]*dx[1] + dx[2]*dx[2] <= bond_length[atom_types[i]-1][atom_types[j]-1]*bond_length[atom_types[i]-1][atom_types[j]-1]:
+                    neighbor_list_test[i].append(j)
+                    neighbor_list_test[j].append(i)
+
+        for idx in range(len(neighbor_list_test)):
+            neighbor_list_test[idx] = sorted(neighbor_list_test[idx])
+
+        return neighbor_list_test      
+#---------------------------------------------------------------------------------------------------------  
+##### ここより下は消す #####                                           
     def get_neighbor_list(self, cut_off: float=3.4 ,bond_length: list[list[float]] = [])->list[list[int]]:
         """neighbor_list を作成します。
         Parameters
@@ -77,40 +247,6 @@ class AnalizeFrame(
                                    cell = self.cell,
                                    mode = "edge")
         return edge_idx
-        
-#-------------------------------------------------------------------------------------    
-    def get_neighbor_list_test(self, bond_length: list[list[float]])->list[list[int]]:
-        """ neighbor_listを作成します。
-            pythonでO(N^2)のため、get_neighbor_list()のtest用です。
-        Parameters
-        -----------
-            bond_length: list[list[float]]
-        """
-        neighbor_list_test = [[] for _ in range(len(self))]
-        x = self.atoms['x'].values
-        y = self.atoms['y'].values
-        z = self.atoms['z'].values
-        atom_types = self.atoms['type'].to_list()
-        for i  in range(self.get_total_atoms()):
-            for j in range(i+1, self.get_total_atoms()):
-                dx:list[float] = [None,None,None]
-                dx[0] = x[j] - x[i]
-                dx[1] = y[j] - y[i]
-                dx[2] = z[j] - z[i]
-                for ax in range(3):
-                    if dx[ax] < -self.cell[ax]/2:
-                        dx[ax] += self.cell[ax]
-                    elif self.cell[ax]/2 < dx[ax]:
-                        dx[ax] -= self.cell[ax]
-                if dx[0]*dx[0] + dx[1]*dx[1] + dx[2]*dx[2] <= bond_length[atom_types[i]-1][atom_types[j]-1]*bond_length[atom_types[i]-1][atom_types[j]-1]:
-                    neighbor_list_test[i].append(j)
-                    neighbor_list_test[j].append(i)
-
-        for idx in range(len(neighbor_list_test)):
-            neighbor_list_test[idx] = sorted(neighbor_list_test[idx])
-
-        return neighbor_list_test
-    
 #--------------------------------------------------------------------------------------    
     def count_molecules(self, cut_off: float=3.4 ,bond_length: list[list[float]] = [])->dict[str, int]:
         """
