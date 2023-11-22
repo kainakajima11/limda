a76cafe69d90d2265776932ebe3004c402da61ca
kudoryutaro <ryutaro.kudo.p2@dc.tohoku.ac.jp>
2023-11-22 14:54:18 +0900

    fixed

--- src/limda/calculate.py
+++ src/limda/calculate.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 import subprocess
 import os
 import time
-from typing import Union
+from typing import Union, Dict
 import torch
 
 try:
@@ -411,7 +411,7 @@ class Calculate(
                 device: Union[str, torch.DeviceObjType],
                 allegro_model: torch.jit._script.RecursiveScriptModule,
                 flag_calc_virial = False,
-                ) -> None:
+                ) -> Dict[str, torch.Tensor]:
         """Allegroを使って、sfに入っている原子の座標に対して推論を行い、
         ポテンシャルエネルギーと原子に働く力を計算する
         Allegroによって予測されたポテンシャルエネルギーはsf.pred_potential_energyに入る
@@ -461,3 +461,4 @@ class Calculate(
         if flag_calc_virial:
             self.pred_virial_tensor = output['virial'].cpu().detach().numpy()
 
+        return output
